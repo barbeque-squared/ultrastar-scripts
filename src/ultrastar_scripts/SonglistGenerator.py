e080f53981763a9c0e72891d12726fd3ea326134
barbeque-squared <barbeque.squared@gmail.com>
2020-11-15 09:44:19 +0100

    add language

--- src/ultrastar_scripts/SonglistGenerator.py
+++ src/ultrastar_scripts/SonglistGenerator.py
@@ -17,14 +17,16 @@ class SongType(IntFlag):
 
 
 class SonglistEntry:
-    def __init__(self, artist: str, title: str, songtype: SongType):
+    def __init__(self, artist: str, title: str, language: str, songtype: SongType):
         self.artist = artist
         self.title = title
+        self.language = language
         self.variants = [songtype]
 
     def __iter__(self):
         yield 'artist', self.artist
         yield 'title', self.title
+        yield 'language', self.language
         yield 'variants', self.variants
 
     def addVariant(self, songtype: SongType):
@@ -44,9 +46,11 @@ class SonglistGenerator:
                 if song:
                     identifier = song.identifier()
                     if identifier in self.__songlist:
+                        if self.__songlist[identifier].language != song.language:
+                            sys.stderr.write(str(p) + ' uses different language\n')
                         self.__songlist[identifier].addVariant(song.songtype)
                     else:
-                        self.__songlist[identifier] = SonglistEntry(song.artist, song.title, song.songtype)
+                        self.__songlist[identifier] = SonglistEntry(song.artist, song.title, song.language, song.songtype)
                 else:
                     sys.stderr.write(str(p) + ' does not look like a song file, skipping\n')
 
@@ -58,6 +62,7 @@ class SonglistGenerator:
         writer.writerow([
             'Artist',
             'Title',
+            'Language',
             'Lossy',
             'Lossy Instrumental',
             'Lossy Duet',
@@ -71,6 +76,7 @@ class SonglistGenerator:
             writer.writerow([
                 entry.artist,
                 entry.title,
+                entry.language,
                 int(SongType.LOSSY in entry.variants),
                 int(SongType.INSTRUMENTAL in entry.variants),
                 int(SongType.DUET in entry.variants),
@@ -84,6 +90,7 @@ class SonglistGenerator:
     def _loadSong(self, path: str):
         artist = None
         title = None
+        language = None
         with open(path) as reader:
             try:
                 for line in reader:
@@ -94,15 +101,25 @@ class SonglistGenerator:
                         artist = line.strip().replace('#ARTIST:', '', 1)
                     elif line.startswith('#TITLE:'):
                         title = line.strip().replace('#TITLE:', '', 1)
-                    if artist is not None and title is not None:
-                        return self._Song(artist, title)
+                    elif line.startswith('#LANGUAGE:'):
+                        language = line.strip().replace('#LANGUAGE:', '', 1)
+                    if artist is not None and title is not None and language is not None:
+                        return self._Song(artist, title, language)
+                    elif not line.startswith('#') and (artist is not None or title is not None or language is not None):
+                        if artist is None:
+                            raise Exception(str(path) + ' does not set #ARTIST')
+                        elif title is None:
+                            raise Exception(str(path) + ' does not set #TITLE')
+                        elif language is None:
+                            raise Exception(str(path) + ' does not set #LANGUAGE')
             except UnicodeDecodeError as ude:
                 raise Exception('error while loading ' + str(path)) from ude
 
     class _Song:
-        def __init__(self, artist: str, title: str):
+        def __init__(self, artist: str, title: str, language: str):
             songtype = SongType.LOSSY
             self.artist = artist
+            self.language = language
             # parse title
             types = {
                 '(Lossless)': SongType.LOSSLESS,
