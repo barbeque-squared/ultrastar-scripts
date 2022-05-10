25f8fabcf783b7da6860a2547d4c0501f3043ba8
barbeque-squared <barbeque.squared@gmail.com>
2022-05-10 16:16:39 +0200

    improve error message

--- src/ultrastar_scripts/libultrastar.py
+++ src/ultrastar_scripts/libultrastar.py
@@ -9,7 +9,7 @@ def minute_fraction_between_beats(firstBeat: int, secondBeat: int, bpm: float) -
     if bpm <= 0:
         raise ValueError('BPM must be positive')
     elif secondBeat < firstBeat:
-        raise ValueError('secondBeat must be equal to or greater than firstBeat')
+        raise ValueError('secondBeat (' + str(secondBeat) + ') must be equal to or greater than firstBeat (' + str(firstBeat) + ')')
     return (secondBeat - firstBeat) / (bpm*4)
 
 # output
